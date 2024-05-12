a48694cd858429e7ffc8ad70e370962a0dbadf5d
Quinn Slack <quinn@slack.org>
2024-05-12 16:03:14 +0200

    wip

--- vscode/webviews/mentions/mentionMenu/MentionMenu.test.tsx
+++ vscode/webviews/mentions/mentionMenu/MentionMenu.test.tsx
@@ -27,110 +27,154 @@ const PROVIDER_P2: ContextMentionProviderMetadata = {
     triggerPrefixes: ['t2:'],
 }
 
+const ITEM_FILE1: ContextItem = {
+    type: 'file',
+    uri: URI.file('file1.go'),
+}
+
+const ITEM_FILE2: ContextItem = {
+    type: 'file',
+    uri: URI.file('file2.ts'),
+}
+
 const PROPS: Pick<
     ComponentProps<typeof MentionMenu>,
-    'params' | 'data' | 'updateMentionMenuParams' | 'appendToEditorQuery' | 'selectOptionAndCleanUp'
+    'params' | 'updateMentionMenuParams' | 'appendToEditorQuery' | 'selectOptionAndCleanUp'
 > = {
     params: { query: '', parentItem: null },
-    data: { items: [], providers: [PROVIDER_P1] },
     updateMentionMenuParams: () => {},
     appendToEditorQuery: () => {},
     selectOptionAndCleanUp: () => {},
 }
 
 describe('MentionMenu', () => {
-    describe('top-level', () => {
+    describe('initial states', () => {
         test('loading items', () => {
             const { container } = render(
                 <MentionMenu {...PROPS} data={{ items: undefined, providers: [PROVIDER_P1] }} />
             )
-            expectMenu(container, ['provider p1', 'Loading...'])
+            expectMenu(container, ['>provider p1', 'Loading...'])
         })
 
         test('empty items', () => {
             const { container } = render(
                 <MentionMenu {...PROPS} data={{ items: [], providers: [PROVIDER_P1] }} />
             )
-            expectMenu(container, ['provider p1'])
+            expectMenu(container, ['>provider p1'])
         })
 
-        function renderWithMocks(data: MentionMenuData): {
-            updateMentionMenuParams: Mock
-            appendToEditorQuery: Mock
-            selectOptionAndCleanUp: Mock
-            container: HTMLElement
-        } {
-            const updateMentionMenuParams = vi.fn()
-            const appendToEditorQuery = vi.fn()
-            const selectOptionAndCleanUp = vi.fn()
+        test('with items', () => {
             const { container } = render(
                 <MentionMenu
                     {...PROPS}
-                    data={data}
-                    updateMentionMenuParams={updateMentionMenuParams}
-                    appendToEditorQuery={appendToEditorQuery}
-                    selectOptionAndCleanUp={selectOptionAndCleanUp}
+                    data={{
+                        items: [ITEM_FILE1, ITEM_FILE2],
+                        providers: [PROVIDER_P1],
+                    }}
                 />
             )
-            return {
-                updateMentionMenuParams,
-                appendToEditorQuery,
-                selectOptionAndCleanUp,
-                container,
-            }
-        }
+            expectMenu(container, ['>provider p1', 'item file file1.go', 'item file file2.ts'])
+        })
+    })
 
-        test('select provider with no trigger char', () => {
+    function renderWithMocks(data: MentionMenuData): {
+        updateMentionMenuParams: Mock
+        appendToEditorQuery: Mock
+        selectOptionAndCleanUp: Mock
+        container: HTMLElement
+    } {
+        const updateMentionMenuParams = vi.fn()
+        const appendToEditorQuery = vi.fn()
+        const selectOptionAndCleanUp = vi.fn()
+        const { container } = render(
+            <MentionMenu
+                {...PROPS}
+                data={data}
+                updateMentionMenuParams={updateMentionMenuParams}
+                appendToEditorQuery={appendToEditorQuery}
+                selectOptionAndCleanUp={selectOptionAndCleanUp}
+            />
+        )
+        return {
+            updateMentionMenuParams,
+            appendToEditorQuery,
+            selectOptionAndCleanUp,
+            container,
+        }
+    }
+
+    describe('select provider with no trigger prefix', () => {
+        function doTest(action: (container: HTMLElement) => void) {
             const { updateMentionMenuParams, appendToEditorQuery, selectOptionAndCleanUp, container } =
                 renderWithMocks({ items: [], providers: [PROVIDER_P1] })
-            expectMenu(container, ['provider p1'])
-            fireEvent.click(screen.getByText('provider p1'))
+            expectMenu(container, ['>provider p1'])
+            action(container)
             expect(updateMentionMenuParams).toBeCalledTimes(1)
             expect(updateMentionMenuParams).toBeCalledWith({ parentItem: PROVIDER_P1 })
             expect(appendToEditorQuery).toBeCalledTimes(0)
             expect(selectOptionAndCleanUp).toBeCalledTimes(0)
-        })
+        }
+        test('click', () => doTest(() => fireEvent.click(screen.getByText('provider p1'))))
+        test('keyboard', () =>
+            doTest(container => {
+                // ArrowDown then ArrowUp is a noop, but do it just to test
+                fireEvent.keyDown(container, { key: 'ArrowDown' })
+                fireEvent.keyDown(container, { key: 'ArrowUp' })
+                fireEvent.keyDown(container, { key: 'Enter' })
+            }))
+    })
 
-        test('select provider with trigger prefix', () => {
+    describe('select provider with trigger prefix', () => {
+        function doTest(action: (container: HTMLElement) => void): void {
             const { updateMentionMenuParams, appendToEditorQuery, selectOptionAndCleanUp, container } =
                 renderWithMocks({ items: [], providers: [PROVIDER_P2] })
-            expectMenu(container, ['provider p2'])
-            fireEvent.click(screen.getByText('provider p2'))
+            expectMenu(container, ['>provider p2'])
+            action(container)
             expect(updateMentionMenuParams).toBeCalledTimes(1)
             expect(updateMentionMenuParams).toBeCalledWith({ parentItem: PROVIDER_P2 })
             expect(appendToEditorQuery).toBeCalledTimes(1)
             expect(appendToEditorQuery).toBeCalledWith('t2:')
             expect(selectOptionAndCleanUp).toBeCalledTimes(0)
-        })
-
-        test('with items', () => {
-            const { container } = render(
-                <MentionMenu
-                    {...PROPS}
-                    data={{
-                        items: [{ type: 'file', uri: URI.file('foo.go') }],
-                        providers: [PROVIDER_P1],
-                    }}
-                />
-            )
-            expectMenu(container, ['provider p1', 'item file foo.go'])
-        })
+        }
+        test('click', () => doTest(() => fireEvent.click(screen.getByText('provider p2'))))
+        test('keyboard', () =>
+            doTest(container => {
+                // ArrowDown then ArrowUp is a noop, but do it just to test
+                fireEvent.keyDown(container, { key: 'ArrowDown' })
+                fireEvent.keyDown(container, { key: 'ArrowUp' })
+                fireEvent.keyDown(container, { key: 'Enter' })
+            }))
     })
 
-    describe('file query', () => {
-        test('with items', () => {
-            const { container } = render(
-                <MentionMenu
-                    {...PROPS}
-                    params={{ parentItem: null, query: 'f' }}
-                    data={{
-                        items: [{ type: 'file', uri: URI.file('foo.go') }],
-                        providers: [PROVIDER_P1],
-                    }}
-                />
-            )
-            expectMenu(container, ['provider p1', 'item file foo.go'])
-        })
+    describe('select item', () => {
+        function doTest(action: (container: HTMLElement) => void): void {
+            const { updateMentionMenuParams, appendToEditorQuery, selectOptionAndCleanUp, container } =
+                renderWithMocks({ items: [ITEM_FILE1, ITEM_FILE2], providers: [PROVIDER_P1] })
+            expectMenu(container, ['provider p1', 'item file file1.go', 'item file file2.ts'])
+            action(container)
+            expect(updateMentionMenuParams).toBeCalledTimes(0)
+            expect(appendToEditorQuery).toBeCalledTimes(0)
+            expect(selectOptionAndCleanUp).toBeCalledTimes(1)
+            expect(selectOptionAndCleanUp.mock.lastCall[0].item).toEqual(ITEM_FILE1)
+        }
+        test('click', () => doTest(() => fireEvent.click(screen.getByText('item file file1.go'))))
+        test('keyboard', () =>
+            doTest(container => {
+                fireEvent.keyDown(container, { key: 'ArrowDown' })
+                fireEvent.keyDown(container, { key: 'Enter' })
+            }))
+    })
+
+    test('arrow keys', () => {
+        const { container } = render(
+            <MentionMenu
+                {...PROPS}
+                data={{ items: [ITEM_FILE1, ITEM_FILE2], providers: [PROVIDER_P1] }}
+            />
+        )
+        expectMenu(container, ['>provider p1', 'item file file1.go', 'item file file2.ts'])
+        fireEvent.keyDown(container, { key: 'ArrowDown' })
+        expectMenu(container, ['provider p1', '>item file file1.go', 'item file file2.ts'])
     })
 })
 
@@ -141,10 +185,15 @@ function expectMenu(container: HTMLElement, expectedRows: string[]): void {
     )
     expect.soft(actualRows).toHaveLength(expectedRows.length)
     for (let i = 0; i < Math.max(expectedRows.length, actualRows.length); i++) {
-        const expectedRow = expectedRows.at(i)
+        const { row: expectedRow, isSelected: expectedRowIsSelected } = parseExpectedRow(
+            expectedRows.at(i)
+        )
         const actualRow = actualRows.at(i)
         if (actualRow && expectedRow) {
             expect.soft(actualRow).toHaveTextContent(expectedRow)
+            if (expectedRowIsSelected) {
+                expect.soft(actualRow).toHaveAttribute('aria-selected', 'true')
+            }
         } else if (actualRow) {
             expect.fail(
                 `Expected no row ${i}, but it is present and has content ${JSON.stringify(
@@ -153,6 +202,16 @@ function expectMenu(container: HTMLElement, expectedRows: string[]): void {
             )
         } else {
             expect.fail(`Expected row ${i} with content ${JSON.stringify(expectedRow)}`)
+        }
+    }
+
+    function parseExpectedRow(expectedRow: string | undefined): {
+        row: string | undefined
+        isSelected: boolean
+    } {
+        return {
+            row: expectedRow?.replace(/^>/, ''),
+            isSelected: expectedRow?.startsWith('>') ?? false,
         }
     }
 }
